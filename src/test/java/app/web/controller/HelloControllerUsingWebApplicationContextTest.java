4b9e2f11bf78b7d0d2cb01ea829aa818128fa493
easonlin win7 <easonlin404@gmail.com>
2016-02-23 09:38:59 +0800

    fix unit test

--- src/test/java/app/web/controller/HelloControllerUsingWebApplicationContextTest.java
+++ src/test/java/app/web/controller/HelloControllerUsingWebApplicationContextTest.java
@@ -8,6 +8,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -27,6 +28,7 @@ import app.config.WebConfig;
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {RootConfig.class, WebConfig.class})
+@ActiveProfiles("test")
 @WebAppConfiguration
 public class HelloControllerUsingWebApplicationContextTest {
 
