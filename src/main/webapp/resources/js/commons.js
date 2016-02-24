677b6f13acf4b79c69066df504d098de31bd91e9
easonlin win7 <easonlin404@gmail.com>
2016-02-24 14:43:46 +0800

    update user management page

--- src/main/webapp/resources/js/commons.js
+++ src/main/webapp/resources/js/commons.js
@@ -1,7 +1,23 @@
+var _pathname = window.location.pathname;
 $( function() {
+	//登出
 	$('#layout_page_logout_a').click(function(){
 		$('#layout_page_logout_form').submit();
-		
 	});
+	
+	$(".nav li").removeClass("active");//this will remove the active class from  
+     //previously active menu item 
+	
+	var $sideBarMenu = $('#sideBarMenu');
+	
+	if (_pathname == '/backend/userManagement')
+		$('#userManagement,#userManagementLi', $sideBarMenu).addClass('active');
+	else if (_pathname == '/backend/')
+		$('#dashboard', $sideBarMenu).addClass('active');
+		
+	
+	
+	
+	console.log("path:" + _pathname);
 
 });
