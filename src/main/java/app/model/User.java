e1f59d74251bd1077bdbd8f2ae40ec9fdee72aa9
easonlin win7 <easonlin404@gmail.com>
2016-02-25 17:54:49 +0800

    update User.java

--- src/main/java/app/model/User.java
+++ src/main/java/app/model/User.java
@@ -1,12 +1,19 @@
 package app.model;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name="users")
-public class User {
+public class User implements Serializable{
+
+  /**
+   *
+   */
+  private static final long serialVersionUID = -8152803826331353264L;
 
   @Id
   private String userName;
