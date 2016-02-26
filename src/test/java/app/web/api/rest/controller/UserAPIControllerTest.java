f3e0cea0234ce7bed2d552c4c57ef73a4400a945
easonlin win7 <easonlin404@gmail.com>
2016-02-26 15:13:27 +0800

    update unit test

--- src/test/java/app/web/api/rest/controller/UserAPIControllerTest.java
+++ src/test/java/app/web/api/rest/controller/UserAPIControllerTest.java
@@ -1,7 +1,10 @@
 package app.web.api.rest.controller;
 
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.ArrayList;
@@ -11,6 +14,9 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -53,6 +59,31 @@ public class UserAPIControllerTest {
     mvc.perform(get("/rest/user")) // Perform GET /
     .andExpect(status().isOk());
   }
+
+  @Test
+  public void testListAllUsersPage() throws Exception {
+    int page = 1;
+    List<User> expectedUsers = createUsers();
+
+    Page<User> p  = new PageImpl<User>(expectedUsers);
+
+    when(userRepo.findAll(new PageRequest(page - 1, 20))).thenReturn(p);
+
+    mvc.perform(get("/rest/user/1?size=20")) // Perform GET /
+    .andExpect(status().isOk())
+    .andExpect(jsonPath("$.number", is(0)))
+    .andExpect(jsonPath("$.last", is(true)))
+    .andExpect(jsonPath("$.size", is(0)))
+    .andExpect(jsonPath("$.content", hasSize(6)))
+    .andExpect(jsonPath("$.content[0].userName", is("Eason Lisn 0")))
+    .andExpect(jsonPath("$.content[0].password", is("password0")))
+    .andExpect(jsonPath("$.content[0].enabled", is(false)));
+
+
+
+  }
+
+
 
   private  List<User> createUsers(){
     List<User> users = new ArrayList<User>();
