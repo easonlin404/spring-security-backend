5c075016de3efb1f56ef5f26a9377656cc03b2de
easonlin win7 <easonlin404@gmail.com>
2016-03-03 16:59:09 +0800

    add test data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -23,3 +23,12 @@ insert into users(username,password,enabled) values ('Bob5','123456789',true);
 insert into users(username,password,enabled) values ('Bob6','123456789',true);
 insert into users(username,password,enabled) values ('Bob7','123456789',true);
 insert into users(username,password,enabled) values ('Bob8','123456789',true);
+
+insert into users(username,password,enabled) values ('user','password',true);
+insert into users(username,password,enabled) values ('admin','password',true);
+
+insert into authorities(username,authority) values ('user','ROLE_USER');
+insert into authorities(username,authority) values ('admin','ROLE_USER');
+insert into authorities(username,authority) values ('admin','ROLE_ADMIN');
+
+
